92678ea8dbaedde7071b3bdad1c6ea1a3095bf5b
rtw16 <rtw16@skbl-422-003.ph.ic.ac.uk>
2018-10-11 16:05:21 +0100

    delay subroutine

--- Simple1.asm
+++ Simple1.asm
@@ -11,11 +11,26 @@ start
 	movwf	TRISB, ACCESS	    ; Port C all outputs
 	bra 	test
 loop	movff 	0x06, PORTB
+	call	globaldelay
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	movlw 	0x63
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+	
+globaldelay	movlw	0x02	    ; setting global counter
+		movwf	0x21, ACCESS
+		decfsz	0x21, F, ACCESS
+		call	delay
+	
+delay	movlw	0x02
+	movwf	0x20, ACCESS
+dloop	nop
+	nop
+	nop
+	decfsz	0x20, F, ACCESS
+	bra	dloop
+	return 0
 	
 	end
