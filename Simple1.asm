ce914f19073e020def881663cfc62b3cc17ec630
Rick Wong <>
2018-10-11 23:02:01 +0100

    Add comments

--- Simple1.asm
+++ Simple1.asm
@@ -4,18 +4,18 @@
 	org 0x0
 	goto	start
 	
-	org 0x100		    ; Main code starts here at address 0x100
+	org 0x100		    		; Main code starts here at address 0x100
 
 start
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	movlw 	0x0					; Move literal (0) to WREG
+	movwf	TRISB, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port C all outputs
+	bra 	test				; Branch to test
+loop	movff 	0x06, PORTB		; Move from FREG address 0x06 to PORTB 
+	incf 	0x06, W, ACCESS		; 
+test	movwf	0x06, ACCESS	; Test for end of loop condition. Move from WREF to FREG, at address 0x06
+	movlw 	0x63				; Move literal (0x63) to WREG
+	cpfsgt 	0x06, ACCESS		; Compare FREG at address 0x06 with WREG. If greater, skip next line. Else, branch to loop
+	bra 	loop		    	; Not yet finished goto start of loop again
+	goto 	0x0		    		; Re-run program from start
 	
 	end
