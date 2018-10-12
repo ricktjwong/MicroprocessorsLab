7d718c10934f403937af1a3f2f4a8fed36986528
Rick Wong <rickwtj@gmail.com>
2018-10-12 12:28:18 +0100

    Set PORT E to store a number

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,9 @@
 	org 0x100		    		; Main code starts here at address 0x100
 
 start
-	movlw 	0x0					; Move literal (0) to WREG
-	movwf	TRISB, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port C all outputs
-	bra 	test				; Branch to test
-loop	movff 	0x06, PORTB		; Move from FREG address 0x06 to PORTB 
-	incf 	0x06, W, ACCESS		; 
-test	movwf	0x06, ACCESS	; Test for end of loop condition. Move from WREF to FREG, at address 0x06
-	movlw 	0x63				; Move literal (0x63) to WREG
-	cpfsgt 	0x06, ACCESS		; Compare FREG at address 0x06 with WREG. If greater, skip next line. Else, branch to loop
-	bra 	loop		    	; Not yet finished goto start of loop again
-	goto 	0x0		    		; Re-run program from start
-	
+	movlw 	0x00				; Move literal (0) to WREG
+	movwf	TRISE, ACCESS			; Move WREG literal to FREG, assign file register to TRISE. Port E all outputs
+	movlw	0xA0
+	movwf	0x09
+	movff	0x09, PORTE
 	end
