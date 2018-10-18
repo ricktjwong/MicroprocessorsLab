984c244ecfc43cb54f8ed36eb964cf57cbc8d30e
Rick Wong <rickwtj@gmail.com>
2018-10-18 15:14:09 +0100

    Fix memstore
    - Store 0x05 to PORTE

--- Simple1.asm
+++ Simple1.asm
@@ -8,34 +8,29 @@
 	org 0x100		    		; Main code starts here at address 0x100
 
 start
-	movlw 	0xfc		    ; Move literal (0) to WREG
-	movwf	TRISD, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port C all outputs
-	movlw	0x03
-	movwf	0x06		    ; set 3 into 0x06 for OE1
-	movff	0x06, PORTD
-	movlw 	0x0		    ; Move literal (0) to WREG
-	movwf	TRISE, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port C all outputs
- 	clrf	TRISE
-	movlw	0x05
-	movwf	0x07
-	movff	0x07, PORTE
-	call	delay
-	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
-	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on
-	movlb	0x00 ; set BSR back to Bank 0
-	setf	TRISE ; Tri-state PortE
-	movlw	0x00
-	movwf	0x07
-	movff	0x07, PORTD	    ; Lowers OE1, memory outputs
-	goto	0x0
+	banksel PADCFG1			; PADCFG1 is not in Access Bank
+	bsf	PADCFG1, REPU, BANKED	; PortE pull-ups on	
+	movwf	TRISD, ACCESS		; Move WREG literal to FREG, assign file register to TRISD
+	movwf	TRISC, ACCESS
+	bsf	PORTD, 0	    ; OE    
+	bsf	PORTD, 1	    ; Clock bit
 
+	movlw 	0x0		    ; Move literal (0) to WREG
+	movwf	TRISE, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port E all outputs
+	movlw	0x05
+	movwf	PORTE
+	bcf	PORTD, 1	    ; CP1 goes low
+	call	delay
+	bsf	PORTD, 1	    ; CP1 goes high 
+	setf	TRISE		    ; Tri-state PortE: PORTE is being pulled up - means voltage is higher than 0 but smaller than 5V (1 bit)
+	bcf	PORTD, 0	    ; Lowers OE1, memory outputs
+	movff	PORTE, PORTC 
+	goto	0x0	
+	
 delay	nop			    ; Do nothing for 250ns
 	nop
 	nop
 	nop
-	movlw	0x02		    ; Only lower CP1
-	movwf	0x06		    ; set 3 into 0x06 for OE1
-	movff	0x06, PORTD
 	return 
 	
 	end
