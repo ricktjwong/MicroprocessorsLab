0b2d28fac96ba7f29a12e83d8842c5faad8b67ed
Rick Wong <rickwtj@gmail.com>
2018-10-19 10:26:56 +0100

    Output values from memchip to two ports

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,43 @@
 	org 0x100		    		; Main code starts here at address 0x100
 
 start
-	movlw 	0x0					; Move literal (0) to WREG
-	movwf	TRISB, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port C all outputs
-	bra 	test				; Branch to test
-loop	movff 	0x06, PORTB		; Move from FREG address 0x06 to PORTB 
-	incf 	0x06, W, ACCESS		; 
-test	movwf	0x06, ACCESS	; Test for end of loop condition. Move from WREF to FREG, at address 0x06
-	movlw 	0x63				; Move literal (0x63) to WREG
-	cpfsgt 	0x06, ACCESS		; Compare FREG at address 0x06 with WREG. If greater, skip next line. Else, branch to loop
-	bra 	loop		    	; Not yet finished goto start of loop again
-	goto 	0x0		    		; Re-run program from start
+	banksel PADCFG1			; PADCFG1 is not in Access Bank
+	bsf	PADCFG1, REPU, BANKED	; PortE pull-ups on	
+	movwf	TRISD, ACCESS		; Move WREG literal to FREG, assign file register to TRISD
+	movwf	TRISC, ACCESS
+	movwf	TRISH, ACCESS
+	bsf	PORTD, 0	    ; OE for CHIP1
+	bsf	PORTD, 1	    ; Clock bit fo
+	bsf	PORTD, 6	    ; OE for CHIP 200
+	bsf	PORTD, 7	    ; Clock bit for CHIP 2
+
+	movlw 	0x0		    ; Move literal (0) to WREG
+	movwf	TRISE, ACCESS	    ; Move WREG literal to FREG, assign file register to TRISB. Port E all outputs
+	movlw	0xAA		    ; 0b10101010
+	movwf	PORTE
+	bcf	PORTD, 1	    ; CP1 goes low
+	call	delay
+	bsf	PORTD, 1	    ; CP1 goes high 
+	movlw	0x55		    ; 0b01010101
+	movwf	PORTE
+	bcf	PORTD, 7	    ; CP2 goes low
+	call	delay
+	bsf	PORTD, 7	    ; CP2 goes high 
+	
+	setf	TRISE		    ; Tri-state PortE: PORTE is being pulled up - means voltage is higher than 0 but smaller than 5V (1 bit)
+	bcf	PORTD, 0	    ; Lowers OE1, memory outputs
+	movff	PORTE, PORTC 
+	bsf	PORTD, 0	    ; Raises OE1, memory outputs closed
+	
+	bcf	PORTD, 6	    ; Lowers OE2, memory outputs
+	movff	PORTE, PORTH
+	bsf	PORTD, 6	    ; Raises OE2, memory outputs closed
+	goto	0x0	
+	
+delay	nop			    ; Do nothing for 250ns
+	nop
+	nop
+	nop
+	return 
 	
 	end
