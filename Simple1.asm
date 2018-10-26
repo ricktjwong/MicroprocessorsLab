85a469663a27fa60a597e5ba10b58457ecd0ddfe
rtw16 <rtw16@skbl-422-003.ph.ic.ac.uk>
2018-10-26 11:01:22 +0100

    Keypad output bytes

--- Simple1.asm
+++ Simple1.asm
@@ -10,9 +10,22 @@ start
     banksel	PADCFG1			; PADCFG1 is not in Access Bank
     bsf		PADCFG1, REPU, BANKED	; PortE pull-ups on	
     clrf	LATE			; Clear latch
+    clrf	LATD			; Clear latch
     movlw	0x0F			; 00001111 - 0 sets outputs, 1 as inputs
     movwf	TRISE, ACCESS
+    movlw	0x00
+    movwf	TRISD, ACCESS
     
+    movff	PORTE, 0x20
 
-	
+    clrf	LATE			; Clear latch
+    movlw	0xF0			; 11110000 - 0 sets outputs, 1 as inputs
+    movwf	TRISE, ACCESS
+    
+    movf	PORTE, W
+    addwf	0x20, 1
+    movff	0x20, PORTD
+    
+    goto    0x0
+
     end
